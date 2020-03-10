3082402ccf64c5bcf0611afe2be3ae4e67907944
Dheimison Siqueira <dheimisonlo@gmail.com>
2020-03-09 23:22:49 -0300

    Updates

---
+++ precache-manifest.f245093274d8dfa36bba0ab959c8de16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698375c4c4078316d5111aed6423925a",
+    "url": "/Show-me-cute-cats/index.html"
+  },
+  {
+    "revision": "e0a8c1127ea13ef8f32b",
+    "url": "/Show-me-cute-cats/static/js/2.df9a56d4.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/Show-me-cute-cats/static/js/2.df9a56d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0e1ce28fe599660060",
+    "url": "/Show-me-cute-cats/static/js/main.ce219d52.chunk.js"
+  },
+  {
+    "revision": "27a76c0daa49848d39ea",
+    "url": "/Show-me-cute-cats/static/js/runtime-main.4c7653a8.js"
+  }
+]);
